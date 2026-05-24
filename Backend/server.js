70f8eabe95a3abe087f3b820cb4bfa9fd78436f5
sanjaykumar9318 <sanjayswiz26@gmail.com>
2026-05-24 17:22:08 +0530

    final deployement

--- Backend/server.js
+++ Backend/server.js
@@ -5,15 +5,27 @@ import connectDB from './config/mongodb.js'
 import cookieParser from 'cookie-parser'
 import userRouter from './routes/userRoutes.js'
 import imageRouter from './routes/imageRoutes.js'
+import path from "path"
 
 const app = express()
 const PORT = process.env.PORT
+// for deployement
+const __dirname = path.resolve()
+
 app.use(cors({
   origin: "http://localhost:5173",
   credentials: true
 }));
 app.use(cookieParser());
 app.use(express.json())
+// for deployement
+if(process.env.NODE_ENV==="production"){
+  app.use(express.static(path.join(__dirname,"../Frontend/dist"))) //if in prod make this dist folder as static acid
+  app.get("*",(req, res) => {
+    res.sendFile(path.join(__dirname, "../Frontend/dist/index.html"));
+  });
+}
+
 
 
 
